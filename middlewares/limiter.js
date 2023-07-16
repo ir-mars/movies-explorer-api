11331d3c63f0463d837245c78abb1c2ab7f5408b
Irina Demina <ir-mars@yandex.ru>
2023-07-16 17:26:26 +0600

    сборка проекта

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,6 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
-module.exports = rateLimit({
+const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
+
+module.exports = {
+  limiter
+};
